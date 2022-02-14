6b718ce8677762969034af61337d8600d6b8738f
javierpaniza <javierpaniza>
2022-02-14 20:53:21 +0100

    Upgraded to OpenXava 6.6.2.

--- XavaProjects/src/org/openxava/xavaprojects/tests/IssueTest.java
+++ XavaProjects/src/org/openxava/xavaprojects/tests/IssueTest.java
@@ -21,18 +21,11 @@ public class IssueTest extends ModuleTestBase {
 		setValue("title", "JUnit Incident");
 		String [][] types = {
 			{ "", "" },
-			/* tmr
-			{ "402880426d5f6588016d5f70e3920001", "Bug" },
-			{ "402880426d5f6588016d5f7100650002", "Feature" }
-			*/
-			// tmr ini
 			{ "4028808d7eea19fe017eea61bec90024", "Bug" },
 			{ "4028808d7eea19fe017eea61d4f20025", "Feature" }
-			// tmr fin
 		};
 		assertValidValues("type.id", types);
-		// tmr setValue("type.id", "402880426d5f6588016d5f70e3920001");
-		setValue("type.id", "4028808d7eea19fe017eea61bec90024"); // Bug tmr
+		setValue("type.id", "4028808d7eea19fe017eea61bec90024"); // Bug 
 		setValue("description", "This is a JUnit Incident");
 		assertValidValuesCount("project.id", 2);
 		assertDescriptionValue("project.id", "OpenXava"); 		
@@ -52,64 +45,39 @@ public class IssueTest extends ModuleTestBase {
 
 		String [][] versions = {
 			{ "", "" },
-			/* tmr
-			{ "402880426d5f6588016d5f7135ee0004", "2.0" },
-			{ "402880426d5f6588016d5f7129ce0003", "1.0" }
-			*/
-			// tmr ini
 			{ "4028808d7eea19fe017eea5074130012", "2.0" },
 			{ "4028808d7eea19fe017eea5057f30011", "1.0" }
-			// tmr fin
 		};
 		assertValidValues("version.id", versions);
-		// tmr setValue("version.id", "402880426d5f6588016d5f7129ce0003");		
-		setValue("version.id", "4028808d7eea19fe017eea5057f30011"); // 1.0 tmr
+		setValue("version.id", "4028808d7eea19fe017eea5057f30011"); // 1.0 
 			
 		String [][] plans = {
 			{ "", "" },	
-			/* tmr
-			{ "402880406dfa06e9016dfa0925830003", "Javi 2019.10" },
-			{ "402880406dfa06e9016dfa16f9160006", "Javi 2019.11" }
-			*/
-			// tmr ini
 			{ "4028808d7eea19fe017eea5b5534001e", "Javi 2019.10" },
 			{ "4028808d7eea19fe017eea5b675b001f", "Javi 2019.11" }
-			// tmr fin
 		};
 		assertValidValues("assignedTo.id", plans);
-		// tmr setValue("assignedTo.id", "402880406dfa06e9016dfa16f9160006");		
-		setValue("assignedTo.id", "4028808d7eea19fe017eea5b675b001f"); // 2019.11 tmr
+		setValue("assignedTo.id", "4028808d7eea19fe017eea5b675b001f"); // 2019.11 
 		
 		String [][] status = {
 			{ "", "" },	
-			/* tmr
-			{ "4028b881715acc4301715adab58c0003", "Done" },
-			{ "4028b881715acc4301715aecad7e0004", "Not reproducible" },
-			{ "4028b881715acc4301715ad9ab010002", "Pending" },
-			{ "4028b881715acc4301715af05d8b0005", "Rejected" }
-			*/
-			// tmr ini
 			{ "4028808d7eea19fe017eea2160df0002", "Done" },
 			{ "4028808d7eea19fe017eea2272ae0003", "Not reproducible" },
 			{ "4028808d7eea19fe017eea1e4ffb0001", "Pending" },
 			{ "4028808d7eea19fe017eea2380970004", "Rejected" }
-			// tmr fin
 		};
 		
 		assertValidValues("status.id", status);
-		// tmr assertValue("status.id", "4028b881715acc4301715ad9ab010002");		
-		assertValue("status.id", "4028808d7eea19fe017eea1e4ffb0001"); // Pending tmr
-		
-		// TMR ME QUEDÉ POR AQUÍ, ESTABA ACTUALIZANDO LOS OIDS
+		assertValue("status.id", "4028808d7eea19fe017eea1e4ffb0001"); // Pending 
 		
 		String [][] customers = {
 			{ "", "" },	
-			{ "402880466eae0e5b016eae13f9f70002", "Banco Santander" },
-			{ "402880466eae0e5b016eae13e29b0001", "Oracle Corporation" }
+			{ "4028808d7ef9b160017ef9b24a960001", "Banco Santander" },
+			{ "4028808d7ef9b160017ef9b28a510002", "Oracle Corporation" }			
 		};
 		
 		assertValidValues("customer.id", customers);
-		setValue("customer.id", "402880466eae0e5b016eae13f9f70002");		
+		setValue("customer.id", "4028808d7ef9b160017ef9b24a960001"); // Banco Santander 
 
 		assertValue("hours", "");
 		setValue("minutes", "90");
@@ -137,16 +105,16 @@ public class IssueTest extends ModuleTestBase {
 		execute("List.viewDetail", "row=0");
 		
 		assertValue("title", "JUnit Incident");
-		assertValue("type.id", "402880426d5f6588016d5f70e3920001");
+		assertValue("type.id", "4028808d7eea19fe017eea61bec90024"); // Bug  
 		assertValue("description", "This is a JUnit Incident");
 		assertDescriptionValue("project.id", "OpenXava"); 
 		assertValue("createdBy", "admin");
 		assertValue("createdOn", getCurrentDate()); // If fails revise the serverTimezone in MySQL url
 		assertValue("priority.level", "7"); 
-		assertValue("version.id", "402880426d5f6588016d5f7129ce0003"); 
-		assertValue("assignedTo.id", "402880406dfa06e9016dfa16f9160006");
-		assertValue("status.id", "4028b881715acc4301715ad9ab010002"); 
-		assertValue("customer.id", "402880466eae0e5b016eae13f9f70002");		
+		assertValue("version.id", "4028808d7eea19fe017eea5057f30011"); // 1.0 
+		assertValue("assignedTo.id", "4028808d7eea19fe017eea5b675b001f"); // 2019.11
+		assertValue("status.id", "4028808d7eea19fe017eea1e4ffb0001"); // Pending  
+		assertValue("customer.id", "4028808d7ef9b160017ef9b24a960001"); // Banco Santander
 		assertValue("minutes", "90");
 		assertValue("hours", "1.50");		
 		
@@ -163,14 +131,8 @@ public class IssueTest extends ModuleTestBase {
 		setValue("title", "JUnit Simple Incident");
 		String [][] types = {
 			{ "", "" },
-			/* tmr
-			{ "402880426d5f6588016d5f70e3920001", "Bug" },
-			{ "402880426d5f6588016d5f7100650002", "Feature" }
-			*/
-			// tmr ini
 			{ "4028808d7eea19fe017eea61bec90024", "Bug" },
 			{ "4028808d7eea19fe017eea61d4f20025", "Feature" }
-			// tmr fin
 		};
 		assertValidValues("type.id", types);
 		setValue("type.id", "4028808d7eea19fe017eea61bec90024"); // Bug
@@ -184,8 +146,7 @@ public class IssueTest extends ModuleTestBase {
 		execute("List.viewDetail", "row=0");
 		
 		assertValue("title", "JUnit Simple Incident");
-		// tmr assertValue("type.id", "402880426d5f6588016d5f70e3920001");
-		assertValue("type.id", "4028808d7eea19fe017eea61bec90024"); // tmr
+		assertValue("type.id", "4028808d7eea19fe017eea61bec90024"); 
 		assertValue("description", "This is a JUnit Incident");
 		assertValue("createdBy", "admin");
 		assertValue("createdOn", getCurrentDate());
--- XavaProjects/src/org/openxava/xavaprojects/tests/PlanTest.java
+++ XavaProjects/src/org/openxava/xavaprojects/tests/PlanTest.java
@@ -20,38 +20,23 @@ public class PlanTest extends ModuleTestBase {
 		
 		String [][] workers = {
 			{"", ""},
-			/* tmr
-			{"402880406dfa06e9016dfa0897340001", "Javi"},
-			{"402880406dfea11b016dfef3623f0004", "Pedro"}
-			*/
-			// tmr ini
 			{"4028808d7eea19fe017eea56ed120018", "Javi"},
 			{"4028808d7eea19fe017eea56ffeb0019", "Pedro"}
-			// tmr fin
 		};
 		assertValidValues("worker.id", workers);
-		// tmr setValue("worker.id", "402880406dfea11b016dfef3623f0004"); // Pedro
-		setValue("worker.id", "4028808d7eea19fe017eea56ffeb0019"); // Pedro // tmr
+		setValue("worker.id", "4028808d7eea19fe017eea56ffeb0019"); // Pedro 
 		
 		String [][] periods = {
 			{"", ""},
-			/* tmr
-			{"402880406dfa06e9016dfa08c7f60002", "2019.10"},
-			{"402880406dfa06e9016dfa16c4ff0005", "2019.11"}
-			*/
-			// tmr ini
 			{"4028808d7eea19fe017eea5a84e5001a", "2019.10"},
 			{"4028808d7eea19fe017eea5ab6dc001b", "2019.11"}
-			// tmr fin
 		};
 		assertValidValues("period.id", periods);
-		// tmr setValue("period.id", "402880406dfa06e9016dfa16c4ff0005"); // 2019.11
-		setValue("period.id", "4028808d7eea19fe017eea5ab6dc001b"); // 2019.11 // tmr
+		setValue("period.id", "4028808d7eea19fe017eea5ab6dc001b"); // 2019.11 
 		
 		execute("Collection.new", "viewObject=xava_view_issues");
 		setValue("title", "The first step of my big plan");
-		// tmr setValue("type.id", "402880426d5f6588016d5f70e3920001"); // Bug
-		setValue("type.id", "4028808d7eea19fe017eea61bec90024"); // Bug // tmr
+		setValue("type.id", "4028808d7eea19fe017eea61bec90024"); // Bug 
 		execute("Collection.save");
 		assertNoErrors();
 		assertCollectionRowCount("issues", 1);
@@ -62,14 +47,8 @@ public class PlanTest extends ModuleTestBase {
 		assertValueInList(1, 1, "2019.11");
 		
 		execute("List.viewDetail", "row=1"); 
-		/* tmr
-		assertValue("worker.id", "402880406dfea11b016dfef3623f0004"); // Pedro
-		assertValue("period.id", "402880406dfa06e9016dfa16c4ff0005"); // 2019.11
-		*/
-		// tmr ini
 		assertValue("worker.id", "4028808d7eea19fe017eea56ffeb0019"); // Pedro
 		assertValue("period.id", "4028808d7eea19fe017eea5ab6dc001b"); // 2019.11		
-		// tmr fin
 		assertCollectionRowCount("issues", 1);
 		assertValueInCollection("issues", 0, 0, "The first step of my big plan");
 		execute("Collection.removeSelected", "row=0,viewObject=xava_view_issues");
